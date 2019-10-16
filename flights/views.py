764b02e7a1a2620e2b27ec9e879d95cbd2dc8825
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-10-16 18:54:03 +0300

    Fay + Feras

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,15 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import FlightListSerializer, BookingListSerializer
+from datetime import datetime
+
+
+todays_date = datetime.now()
+
+class FlightList(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightListSerializer
+
+class BookingList(ListAPIView):
+	queryset = Booking.objects.filter(date__gte=todays_date)
+	serializer_class = BookingListSerializer
